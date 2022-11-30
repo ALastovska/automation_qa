c3b28327a480284b3c1ee6dafbc31d02595f887b
ALastovska <aiga.lastovska@gmail.com>
2022-11-30 11:22:53 +0200

    [master]: Add classroom progress 29.11

--- src/test/java/TestSauceDemo.java
+++ src/test/java/TestSauceDemo.java
@@ -1,11 +1,17 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import lv.acodemy.constants.Generic;
 import lv.acodemy.page_object.InventoryPage;
 import lv.acodemy.page_object.LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 import static lv.acodemy.constants.Generic.SAUCE_URL;
 
@@ -15,18 +21,33 @@ public class TestSauceDemo {
     LoginPage loginPage;
     InventoryPage inventoryPage;
 
-    @BeforeMethod
+    @BeforeMethod(description = "Preconditions")
     public void initialize() {
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+
         driver = new ChromeDriver();
         loginPage = new LoginPage(driver);
         inventoryPage = new InventoryPage(driver);
+        driver.get(SAUCE_URL);
     }
 
-    @Test
+    @Test(description = "Happy path: Test authorization with standard user")
     public void authorizeTest() {
         driver.get(SAUCE_URL);
         loginPage.authorize("standard_user", "secret_sauce");
         Assert.assertEquals(inventoryPage.getTitleElement().getText(), "PRODUCTS");
+    }
+
+    @Test(description = "Failure: Test authorization error message with incorrect credentials")
+    public void openProductTest() {
+        driver.get(SAUCE_URL);
+        loginPage.authorize("standard_user", "secret_sauce");
+        Assert.assertEquals(inventoryPage.getTitleElement().getText(), "PRODUCTS");
+        inventoryPage.clicklOnProductByLabel("Sauce Labs Bolt T-Shirt");
+        System.out.println();
     }
 
     @AfterMethod
